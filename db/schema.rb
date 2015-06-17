14fb9393f2d1b37deeaeb81fe1f3ce99a97cf16a
zunda <zundan@gmail.com>
2015-06-16 17:05:15 -1000

    $ bundle exec rake db:migrate
    == 20150617030348 CreateTicks: migrating ======================================
    -- create_table(:ticks)
       -> 0.0191s
    == 20150617030348 CreateTicks: migrated (0.0192s) =============================

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150617030348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ticks", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "client_time"
+    t.datetime "server_time"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
